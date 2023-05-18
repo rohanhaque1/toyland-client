aedb1cde50d4787566ceadedacb19d815efa6028
rohan-haque <rohanhaque234@gmail.com>
2023-05-18 11:18:08 +0600

    navar added to the website

--- src/components/HeaderAndFooter/Header.jsx
+++ src/components/HeaderAndFooter/Header.jsx
@@ -1,11 +1,85 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <>
-            
-        </>
-    );
+  return (
+    <>
+      <div className="navbar flex justify-between bg-white md:px-20 py-4">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-bold text-md"
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/allToys">All Toys</Link>
+              </li>
+              <li>
+                <Link to="/blogs">Blogs</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </div>
+          <Link to="/" className="font-bold text-3xl">
+            <div className="flex gap-2">
+              <img
+                src="https://images.pexels.com/photos/1592384/pexels-photo-1592384.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+                className="w-9 h-9 rounded-full"
+              />
+              <h2>Toy_Land</h2>
+            </div>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 font-bold text-md">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/allToys">All Toys</Link>
+            </li>
+            <li>
+              <Link to="/blogs">Blogs</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="w-12 rounded-full">
+          <div className="bg-neutral-focus text-neutral-content rounded-full w-12">
+            <img
+              className="cursor-pointer w-12 h-12 rounded-full"
+              title=""
+              src=""
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
