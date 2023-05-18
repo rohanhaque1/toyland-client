08c2a26860d13343d87cc52fbce3c8c479ce4a40
rohan-haque <rohanhaque234@gmail.com>
2023-05-18 14:16:53 +0600

    hide firebase config

--- src/components/firebase/firebase.config.js
+++ src/components/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA5EeuqX1GMHIlFYanQ1-sUjkEF0_NztvE",
-  authDomain: "toyland-shop-client.firebaseapp.com",
-  projectId: "toyland-shop-client",
-  storageBucket: "toyland-shop-client.appspot.com",
-  messagingSenderId: "868065715655",
-  appId: "1:868065715655:web:a3f3176677117c3802a5e2",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
