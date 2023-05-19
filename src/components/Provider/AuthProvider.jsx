662955bcafd4d19b02226d4eb846702cb0be72ae
rohan-haque <rohanhaque234@gmail.com>
2023-05-20 01:36:50 +0600

    a loader spinner will show wjile loading page

--- src/components/Provider/AuthProvider.jsx
+++ src/components/Provider/AuthProvider.jsx
@@ -17,29 +17,35 @@ const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loader, setLoader] = useState(true);
 
   // create a user with email and password
   const createUser = (email, password) => {
+    setLoader(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   // display user photo
   const userProfile = (profile) => {
+    setLoader(true);
     return updateProfile(auth.currentUser, profile);
   };
 
   // signin a user with eemail and password
   const signInUser = (email, password) => {
+    setLoader(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   // google sign in
   const googleSignIn = () => {
+    setLoader(true);
     return signInWithPopup(auth, googleProvider);
   };
 
   // user sign out
   const signOutUser = () => {
+    setLoader(true);
     return signOut(auth)
   }
 
@@ -47,6 +53,7 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoader(false)
     });
 
     return () => {
@@ -56,6 +63,7 @@ const AuthProvider = ({ children }) => {
 
   const authInfo = {
     user,
+    loader,
     createUser,
     userProfile,
     signInUser,
--- src/components/routes/PrivateRoutes.jsx
+++ src/components/routes/PrivateRoutes.jsx
@@ -1,11 +1,27 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Provider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
+import { Dna } from 'react-loader-spinner';
 
 const PrivateRoutes = ({children}) => {
-    const { user } = useContext(AuthContext)
+    const { user, loader } = useContext(AuthContext)
     const location = useLocation()
     
+    if (loader) {
+        return (
+          <div className="text-center w-1/6 mx-auto">
+            <Dna
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="dna-loading"
+              wrapperStyle={{}}
+              wrapperClass="dna-wrapper"
+            />
+          </div>
+        );
+    }
+
 
     if (user) {
         return children;
